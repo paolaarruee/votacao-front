b45a86c59b678425fd10990b9553fa06303bba53
Paola <paolaarruee@gmail.com>
2024-05-12 08:14:30 -0300

    feat: menu mobile

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { ListaPautasComponent } from './lista-pautas/lista-pautas.component';
 
 @NgModule({
   declarations: [NavbarComponent, ListaPautasComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [NavbarComponent, ListaPautasComponent],
 })
 export class ComponentModule {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @ViewChild('menuNav', { static: true })
+  menuNavRef!: ElementRef;
 
+  toggleMenu() {
+    const menuNav = this.menuNavRef.nativeElement;
+    if (menuNav) {
+      menuNav.classList.toggle('active');
+    }
+  }
 }
