b69df08f5f66710986b365b75647df46bb5eeab6
Paola <paolaarruee@gmail.com>
2024-05-14 19:17:26 -0300

    adicionando testes unitarios

--- src/app/components/pauta/register-pauta/register-pauta.component.spec.ts
+++ src/app/components/pauta/register-pauta/register-pauta.component.spec.ts
@@ -8,31 +8,72 @@ import { of } from 'rxjs';
 import { Pauta } from 'src/app/shared/interfaces/pauta';
 import { SessaoVotacao } from 'src/app/shared/interfaces/sessao-votacao';
 
-describe('RegisterPautaComponent', () => {
+describe('Componente RegisterPauta', () => {
   let component: RegisterPautaComponent;
   let fixture: ComponentFixture<RegisterPautaComponent>;
-  let pautaServiceSpy: jasmine.SpyObj<PautaService>;
+  let pautaService: jasmine.SpyObj<PautaService>;
 
-  beforeEach(() => {
-    const spy = jasmine.createSpyObj('PautaService', [
+  beforeEach(async () => {
+    pautaService = jasmine.createSpyObj('PautaService', [
       'createPauta',
       'createSessao',
     ]);
 
-    TestBed.configureTestingModule({
+    await TestBed.configureTestingModule({
       declarations: [RegisterPautaComponent],
-      imports: [ReactiveFormsModule, HttpClientTestingModule],
-      providers: [{ provide: PautaService, useValue: spy }],
-    });
-
-    fixture = TestBed.createComponent(RegisterPautaComponent);
-    component = fixture.componentInstance;
-    pautaServiceSpy = TestBed.inject(
-      PautaService
-    ) as jasmine.SpyObj<PautaService>;
+      imports: [ReactiveFormsModule],
+      providers: [{ provide: PautaService, useValue: pautaService }],
+    }).compileComponents();
   });
 
-  it('should create', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterPautaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve enviar o formulário com sucesso', () => {
+    spyOn(Math, 'random').and.returnValue(0.5);
+
+    const mockPauta: Pauta = {
+      id: 500,
+      titulo: 'Título de Teste',
+      descricao: 'Descrição de Teste',
+    };
+    const mockSessao: SessaoVotacao = {
+      pautaId: 500,
+      nomeSessao: 'Sessão de Teste',
+      dataInicio: '2024-05-14 00:00:00',
+      duracaoMinutos: 60,
+    };
+
+    pautaService.createPauta.and.returnValue(of(mockPauta));
+    pautaService.createSessao.and.returnValue(of(mockSessao));
+
+    component.registerPauta.patchValue({
+      titulo: 'Título de Teste',
+      descricao: 'Descrição de Teste',
+      nomeSessao: 'Sessão de Teste',
+      dataInicio: '2024-05-14',
+      duracaoMinutos: '60',
+    });
+
+    component.onSubmit();
+
+    expect(pautaService.createPauta).toHaveBeenCalledWith({
+      id: 500,
+      titulo: 'Título de Teste',
+      descricao: 'Descrição de Teste',
+    });
+    expect(pautaService.createSessao).toHaveBeenCalledWith(500, {
+      pautaId: 500,
+      nomeSessao: 'Sessão de Teste',
+      dataInicio: '2024-05-14 00:00:00',
+      duracaoMinutos: 60,
+    });
   });
 });
