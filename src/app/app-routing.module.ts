38bb741a15e9264ff343817161aa61280ef6410b
Paola <paolaarruee@gmail.com>
2024-05-14 18:46:23 -0300

    feat:Teste unitarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaPautasComponent } from './components/pauta/lista-pautas/lista-pautas.component';
+import { RegisterPautaComponent } from './components/pauta/register-pauta/register-pauta.component';
 
 const routes: Routes = [
   // { path: '', component: HomeComponent },
   // { path: 'entrar', component: LoginComponent },
   // { path: 'cadastrar', component: RegisterUser },
-  // { path: 'cadastrar-pauta', component: RegisterPautaComponent },
+  { path: 'cadastrar-pauta', component: RegisterPautaComponent },
   { path: 'lista-pautas', component: ListaPautasComponent },
 ];
 
--- src/app/core/services/pauta.service.spec.ts
+++ src/app/core/services/pauta.service.spec.ts
@@ -1,16 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { PautaService } from './pauta.service';
+import { Pauta } from 'src/app/shared/interfaces/pauta';
 
 describe('PautaService', () => {
   let service: PautaService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PautaService],
+    });
     service = TestBed.inject(PautaService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve retornar pautas e totalCount corretos', () => {
+    const page = 1;
+    const limit = 10;
+    const mockPautas: Pauta[] = [
+      { id: 1, titulo: 'Pauta 1', descricao: 'Descricao Pauta 1' },
+      { id: 2, titulo: 'Pauta 2', descricao: 'Descricao Pauta 2' },
+    ];
+    const mockTotalCount = 20;
+
+    service.getPautas(page, limit).subscribe((result) => {
+      expect(result.pautas).toEqual(mockPautas);
+      expect(result.totalCount).toBe(mockTotalCount);
+    });
+
+    const req = httpTestingController.expectOne(
+      `http://localhost:3333/pautas?page=${page}&limit=${limit}`
+    );
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockPautas, {
+      headers: { 'x-total-count': mockTotalCount.toString() },
+    });
   });
 });
