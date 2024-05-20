2c143a41572d69a08507cca7af2871ce0f6c63ab
Paola <paolaarruee@gmail.com>
2024-05-20 06:02:30 -0300

    feat: estilo do filtro e responsividade

--- src/app/shared/interfaces/pauta.ts
+++ src/app/shared/interfaces/pauta.ts
@@ -9,8 +9,19 @@ export interface Pauta {
 }
 
 export enum CategoriaPauta {
-  CATEGORIA_1 = '1',
-  CATEGORIA_2 = '2'
+  CATEGORIA_1 = 'Agricultura e Pecuária',
+  CATEGORIA_2 = 'Ciência e Tecnologia',
+  CATEGORIA_3 = 'Comunicação e Mídia',
+  CATEGORIA_4 = 'Cultura e Lazer',
+  CATEGORIA_5 = 'Direitos Humanos e Justiça',
+  CATEGORIA_6 = 'Economia e Finanças',
+  CATEGORIA_7 = 'Educação',
+  CATEGORIA_8 = 'Habitação e Urbanismo',
+  CATEGORIA_9 = 'Infraestrutura e Transportes',
+  CATEGORIA_10 = 'Meio Ambiente',
+  CATEGORIA_11 = 'Política e Governança',
+  CATEGORIA_12 = 'Saúde',
+  CATEGORIA_13 = 'Segurança Pública',
 }
 
 export interface FiltrosPauta {
